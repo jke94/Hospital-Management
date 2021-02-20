9e9ab438600604d26658ff684a37c98991ebf626
jke94 <jcarracedo.dev@gmail.om>
2021-02-20 19:06:57 +0100

    NuGets and dbcontext

---
+++ DataModel/Context/HospitalDataContext.cs
@@ -0,0 +1,14 @@
+﻿using DataModel.Models;
+using Microsoft.EntityFrameworkCore;
+namespace DataModel.Context
+{
+    public class HospitalDataContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Hospital");
+        }
+    }
+}
---
+++ DataModel/Models/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Models
+{
+    public class Doctor
+    {
+        public int DoctorID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
